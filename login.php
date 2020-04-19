eae99f8e119c1a26a785bad03c50a70604bbc30a
migniearj <migniearj@alum.us.es>
2020-04-19 17:49:40 +0200

    hfg

--- login.php
+++ login.php
@@ -3,22 +3,21 @@
 	session_start();
   	
   	include_once("gestionBD.php");
- 	include_once("gestionarUsuarios.php");
+ 	include_once("gestionUsuario.php");
 	
 	if (isset($_POST['submit'])){
-		$email= $_POST['email'];
 		$user = $_POST['user'];
 		$pass = $_POST['pass'];
 
 		$conexion = crearConexionBD();
-		$num_usuarios = consultarUsuario($conexion,$email,$user,$pass);
+		$num_usuarios = consultarUsuario($conexion,$user,$pass);
 		cerrarConexionBD($conexion);	
 	
 		if ($num_usuarios == 0)
 			$login = "error";	
 		else {
-			$_SESSION['login'] = $email;
-			Header("Location: index.php");
+			$_SESSION['login'] = $user;
+			Header("Location: principal.php");
 		}	
 	}
 
@@ -35,7 +34,7 @@
 <body>
 
 <?php
-	include_once("cabeceraprincipal.php");
+	include_once("Cabecera.php");
 ?>
 
 <main>
@@ -48,15 +47,13 @@
 	<!-- The HTML login form -->
 	<br></br>	
 	<form action="login.php" method="post">
-		<div><label for="email">Email: </label><input type="text" name="email" id="email" /></div>
-		<p></p>	
 		<div><label for="email">Usuario: </label><input type="text" name="user" id="user" /></div>
 		<p></p>	
 		<div><label for="pass">Contraseña: </label><input type="password" name="pass" id="pass" /></div>
 		<input type="submit" name="submit" value="submit" />
 	</form>
 		
-	<p>¿No estás registrado? <a href="altaUsuario.php">¡Registrate!</a></p>
+	<p>¿No estás registrado? <a href="FormAltaUsuario.php">¡Registrate!</a></p>
 </main>
 <?php
 	include_once("pieproyecto.php");
