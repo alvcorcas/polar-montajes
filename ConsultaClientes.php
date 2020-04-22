8965cd4ac7c087fd5e03fc3f20d385aed78691e8
migniearj <migniearj@alum.us.es>
2020-04-22 14:04:27 +0200

    S

--- ConsultaClientes.php
+++ ConsultaClientes.php
@@ -71,8 +71,8 @@ include_once ("cabecera.php");
 <main>
 	
 	<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
+  <li><a href= Principal.php >Polar Montajes:</a></li>
+  <li><a href="#news">Servicio</a></li>
   <li><a href="#contact">Contact</a></li>
   <li><a href="#about">About</a></li>
 	</ul>
